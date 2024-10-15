083e21a5a75dea224ce79ae674951f80c04c98fc
Adrien Marré <marre.cned@yahoo.com>
2024-10-15 14:36:18 +0200

    Optimisation du code en n'écrivant qu'une fois la création des deux nombres aléatoires

--- Calcul Git 2/Program.cs
+++ Calcul Git 2/Program.cs
@@ -33,11 +33,12 @@ namespace Calcul_Git_2
                 // traitement des choix
                 if (choix != 0)
                 {
+                    // Création des 2 nombres aléatoires
+                    val1 = rand.Next(1, 10);
+                    val2 = rand.Next(1, 10);
                     if (choix == 1)
                     {
                         // choix de l'addition
-                        val1 = rand.Next(1, 10);
-                        val2 = rand.Next(1, 10);
                         // saisie de la réponse
                         Console.Write(val1 + " + " + val2 + " = ");
                         reponse = int.Parse(Console.ReadLine());
@@ -55,8 +56,6 @@ namespace Calcul_Git_2
                     else
                     {
                         // choix de la multiplication
-                        val1 = rand.Next(1, 10);
-                        val2 = rand.Next(1, 10);
                         // saisie de la réponse
                         Console.Write(val1 + " x " + val2 + " = ");
                         reponse = int.Parse(Console.ReadLine());
